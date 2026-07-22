cf51dd309050078a421ac730bb9f2e9d1d02b146
Rcheu <rcheung1997@gmail.com>
2026-07-21 22:31:26 -0700

    fixed indentation + rearranged HTTP request checks

--- tcp_server.py
+++ tcp_server.py
@@ -37,27 +37,31 @@ def start_tcp_server():
     serverSocket.listen(1)
     print("The server is ready to receive")
 
-while True:
-    connectionSocket, addr = serverSocket.accept()
-    print("Connection received from:", addr)
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        print("Connection received from:", addr)
 
-    request = connectionSocket.recv(1024).decode()
-    print(request)
-    # sentence = connectionSocket.recv(1024).decode()
-    # capitalizedSentence = sentence.upper()
+        request = connectionSocket.recv(1024).decode()
+        print(request)
+        # sentence = connectionSocket.recv(1024).decode()
+        # capitalizedSentence = sentence.upper()
 
-    # 304 - Not Modified; when user request has "If-Modified-Since"
+        # 505 - HTTP Version Not Supported; when user request is not HTTP1.1
 
-    # 403 - Forbidden; when user request does not have permission to access
 
-    # 404 - Not Found; when user request for something that does not exist
+        # 404 - Not Found; when user request for something that does not exist
+        
 
-    # 505 - HTTP Version Not Supported; when user request is not HTTP1.1
+        # 403 - Forbidden; when user request does not have permission to access
 
-    # 200 - OK; everything checks out correctly
-    responseStatus = "HTTP/1.1 200 OK"
-    connectionSocket.send(responseStatus.encode())
-    # connectionSocket.send(capitalizedSentence.encode())
+
+        # 304 - Not Modified; when user request has "If-Modified-Since"
+
+
+        # 200 - OK; everything checks out correctly
+        responseStatus = "HTTP/1.1 200 OK"
+        connectionSocket.send(responseStatus.encode())
+        # connectionSocket.send(capitalizedSentence.encode())
 
         connectionSocket.close()
 
