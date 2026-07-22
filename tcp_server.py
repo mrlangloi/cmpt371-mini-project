e95b5a9fe4ff4ccafdec407f3d35d1000dd6dd11
Rcheu <rcheung1997@gmail.com>
2026-07-21 21:49:00 -0700

    created implementation skeleton for HTTP status codes

--- tcp_server.py
+++ tcp_server.py
@@ -7,8 +7,25 @@ serverSocket.listen(1)
 print("The server is ready to receive")
 
 while True:
-	connectionSocket, addr = serverSocket.accept()
-	sentence = connectionSocket.recv(1024).decode()
-	capitalizedSentence = sentence.upper()
-	connectionSocket.send(capitalizedSentence.encode())
-	connectionSocket.close()
+    connectionSocket, addr = serverSocket.accept()
+    print("Connection received from:", addr)
+
+    request = connectionSocket.recv(1024).decode()
+    print(request)
+    # sentence = connectionSocket.recv(1024).decode()
+    # capitalizedSentence = sentence.upper()
+
+    # 304 - Not Modified; when user request has "If-Modified-Since"
+
+    # 403 - Forbidden; when user request does not have permission to access
+
+    # 404 - Not Found; when user request for something that does not exist
+
+    # 505 - HTTP Version Not Supported; when user request is not HTTP1.1
+
+    # 200 - OK; everything checks out correctly
+    responseStatus = "HTTP/1.1 200 OK"
+    connectionSocket.send(responseStatus.encode())
+    # connectionSocket.send(capitalizedSentence.encode())
+
+    connectionSocket.close()
