ed967469c780db18f3993a8df9b9739c1364dcfd
Rcheu <rcheung1997@gmail.com>
2026-08-04 22:15:46 -0700

    added function to get both host and port number

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -3,6 +3,32 @@ import threading
 
 serverPort = 12001
 
+# e.g., b"GET /api/v1 HTTP/1.1\r\nHost: localhost:8080\r\nAccept: */*\r\n\r\n"
+# returns ('localhost', 8080)
+def get_host_port(requestData):
+    # get the value after the "Host:" string
+    hostStart = requestData.find(b'Host: ') + len(b'Host: ')
+    hostEnd = requestData.find(b'\r\n', hostStart)
+    hostString = requestData[hostStart:hostEnd].decode()
+
+    # get the port
+    portStart = hostString.find(':')
+    webserverStart = hostString.find('/')
+
+    if webserverStart == -1:
+        webserverStart = len(hostString)
+
+    if portStart == -1 or webserverStart < portStart:
+        # default port
+        port = 80
+        host = hostString[0:webserverStart]
+    else:
+        # extract the specific port from the host string
+        port = int((hostString[portStart + 1:])[0:webserverStart - portStart - 1])
+        host = hostString[:portStart]
+
+    return host, port
+
 def handle_proxy_request(clientSocket):
     # receive client request
     requestData = clientSocket.recv(1024).decode()
@@ -20,7 +46,9 @@ def handle_proxy_request(clientSocket):
     firstHeader = headers[0].split(' ')
     method, url, version = firstHeader
     print(f"Method: {method}, URL: {url}, Version: {version}")
-    
+
+    host, port = get_host_port(requestData)
+
 
 def start_tcp_proxy():
     serverSocket = socket(AF_INET, SOCK_STREAM)
