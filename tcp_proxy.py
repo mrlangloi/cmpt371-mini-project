b8670d48e1b1763cd393c8912bb5e213d9c1de1b
Rcheu <rcheung1997@gmail.com>
2026-08-04 21:08:20 -0700

    added request parsing

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -2,6 +2,23 @@ from socket import *
 
 serverPort = 12001
 
+def handle_proxy_request(clientSocket):
+    # receive client request
+    requestData = clientSocket.recv(1024).decode()
+
+    if not requestData:
+        return
+
+    print("Request Data:")
+    print(requestData)
+
+    # parse the request
+    headers = requestData.split('\r\n')
+    firstHeader = headers[0].split(' ')
+    method, url, version = firstHeader
+    print(f"Method: {method}, URL: {url}, Version: {version}")
+    
+
 def start_tcp_proxy():
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(("", serverPort))
@@ -11,6 +28,9 @@ def start_tcp_proxy():
     while True:
         connectionSocket, addr = serverSocket.accept()
         print("Connection received from:", addr)
+
+        # process the request
+        handle_proxy_request(connectionSocket)
 
         connectionSocket.close()
 
