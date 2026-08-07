0fd114bba77191aed429027f06991cec935219e3
David Song <101168360+kyumsong@users.noreply.github.com>
2026-08-07 10:34:18 -0700

    Add multi-threading to web server

--- tcp_server.py
+++ tcp_server.py
@@ -1,4 +1,7 @@
 from socket import *
+from datetime import datetime, timezone
+import os
+import threading
 
 # Logic to generate each status code + HTTP request message to test (using `curl -v` in a terminal).
 # -v flag is to set "verbose mode", which shows details of the req/res exchange.
@@ -30,6 +33,76 @@ from socket import *
 
 serverPort = 12000
 
+def handle_client(connectionSocket, addr):
+    print("Connection received from:", addr)
+
+    request = connectionSocket.recv(1024).decode()
+    print(request)
+
+    # Parse the request line 
+    lines = request.split("\r\n")
+    method, path, version = lines[0].split() # lines[0] is the request line
+
+    # Map the URL path to a local file
+    if path == "/":
+        filename = "test.html"
+    else:
+        filename = path.lstrip("/")
+
+    # sentence = connectionSocket.recv(1024).decode()
+    # capitalizedSentence = sentence.upper()
+
+    # 505 - HTTP Version Not Supported; when user request is not HTTP1.1
+    if version != "HTTP/1.1":
+        response = "HTTP/1.1 505 HTTP Version Not Supported\r\n\r\n"
+        connectionSocket.send(response.encode())
+        connectionSocket.close()
+        return
+
+    # 404 - Not Found; when user request for something that does not exist
+    if not os.path.isfile(filename):
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        connectionSocket.send(response.encode())
+        connectionSocket.close()
+        return
+
+    # 403 - Forbidden; when user request does not have permission to access
+    if not os.access(filename, os.R_OK):
+        response = "HTTP/1.1 403 Forbidden\r\n\r\n"
+        connectionSocket.send(response.encode())
+        connectionSocket.close()
+        return
+
+    # 304 - Not Modified; when user request has "If-Modified-Since"
+
+    # Find If-Modified-Since header
+    ims_value = None
+    for line in lines[1:]:
+        if line.lower().startswith("if-modified-since:"):
+            ims_value = line.split(":", 1)[1].strip()
+            break
+
+    if ims_value:
+        ims = datetime.strptime(ims_value, "%a, %d %b %Y %H:%M:%S GMT").replace(tzinfo=timezone.utc)
+        mtime = datetime.fromtimestamp(os.path.getmtime(filename), timezone.utc) # File's actual modified time
+        # The file was last modified at or before the client's date - File hasn't changed since the client's copy
+        if mtime <= ims:
+            response = "HTTP/1.1 304 Not Modified\r\n\r\n"
+            connectionSocket.send(response.encode())
+            connectionSocket.close()
+            return
+
+    # 200 - OK; everything checks out correctly
+    with open(filename, "rb") as f:
+        body = f.read()
+
+    response = "HTTP/1.1 200 OK\r\n"
+    response += "\r\n"
+    connectionSocket.send(response.encode() + body)
+    # connectionSocket.send(capitalizedSentence.encode())
+
+    connectionSocket.close()
+
 def start_tcp_server():
     # create TCP socket
     serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -39,31 +112,8 @@ def start_tcp_server():
 
     while True:
         connectionSocket, addr = serverSocket.accept()
-        print("Connection received from:", addr)
-
-        request = connectionSocket.recv(1024).decode()
-        print(request)
-        # sentence = connectionSocket.recv(1024).decode()
-        # capitalizedSentence = sentence.upper()
-
-        # 505 - HTTP Version Not Supported; when user request is not HTTP1.1
-
-
-        # 404 - Not Found; when user request for something that does not exist
-        
-
-        # 403 - Forbidden; when user request does not have permission to access
-
-
-        # 304 - Not Modified; when user request has "If-Modified-Since"
-
-
-        # 200 - OK; everything checks out correctly
-        responseStatus = "HTTP/1.1 200 OK"
-        connectionSocket.send(responseStatus.encode())
-        # connectionSocket.send(capitalizedSentence.encode())
-
-        connectionSocket.close()
+        clientThread = threading.Thread(target=handle_client, args=(connectionSocket, addr))
+        clientThread.start()
 
 if __name__ == "__main__":
     start_tcp_server()
